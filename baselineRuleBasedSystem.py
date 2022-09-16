d76e9df872b11e906865316986d1215765b1c6f6
Gustavo Luchetti <gustavo@luchetti.dev>
2022-09-16 14:14:03 +0200

    feat: merging first baseline sytem

--- baselineRuleBasedSystem.py
+++ baselineRuleBasedSystem.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[285]:
-
-
-import pandas as pd
-import numpy as np
-from re import search
-import matplotlib.pyplot as plt
-import sklearn.metrics
-
-
 # In[270]:
 
 
@@ -22,240 +12,44 @@ df = df[df.label != 'null']
 df = df.reset_index()
 
 
-# In[286]:
-
-
-def BaselineRuleBased(dataframe):
-    """
-    Rule-based prediction of dialog acts based on utterances.
-    Arguments:
-        dataframe: a pandas dataframe that contains a column named text with utterances.
-        
-    Returns:
-        Returns a list of predictions about the label (dialog act) of the utterances.
-    """
-    predictions = []
-    for i in range(0,len(dataframe)):
-        if search(r'\bhow about\b|\bwhat about\b|\banything else\b|\bare there\b|\bis there\b|\bwhat else\b', df.loc[i,'text']):
-            predictions.append('reqalts')
-        elif search(r'\byes\b|\byeah\b|\bcorrect\b',df.loc[i,'text']):
-            predictions.append('affirm')
-        elif search(r'\bthank you\b', df.loc[i,'text']):
-            predictions.append('thankyou')
-        elif search(r'\bgoodbye\b|\bbye\b', df.loc[i,'text']):
-            predictions.append('bye')
-        elif search(r'\bdoes it\b|\bis it\b|\bdo they\b|\bis that\b|\bis there\b', df.loc[i,'text']):
-            predictions.append('confirm')
-        elif search(r'\bwhat is\b|\bwhats\b|\bmay i\b|\bcould i\b|\bwhat\b|\bprice range\b|\bpost code\b|\btype of\b|\baddress\b|\bphone number\b|\bcan i\b|\bcould i\b|\bcould you\b|\bdo you\b|\bi want+.address\b|\bi want+.phone\b|\bi would\b|\bwhere is\b', df.loc[i,'text']):
-            predictions.append('request')
-        elif search(r'\bno\b|\bnot\b', df.loc[i,'text']):
-            predictions.append('negate')
-        elif search(r'\blooking for\b|\bdont care\b|\bdoesnt matter\b|\bexpensive\b|\bcheap\b|\bmoderate\b|\bi need\b|\bi want\b|\bfood\b|\bnorth\b',df.loc[i,'text']):
-            predictions.append('inform')
-        elif search(r'\bdont\b', df.loc[i,'text']):
-            predictions.append('deny')
-        elif search(r'\bhello\b', df.loc[i,'text']):
-            predictions.append('hello')
-        elif search(r'\brepeat\b', df.loc[i,'text']):
-            predictions.append('repeat')
-        elif search(r'\bmore\b', df.loc[i,'text']):
-            predictions.append('reqmore')
-        elif search(r'\bstart\b', df.loc[i,'text']):
-            predictions.append('restart')
-        elif search(r'\bokay\b|\bkay\b',df.loc[i,'text']):
-            predictions.append('ack')
-        else:
-            predictions.append('inform')
-    return predictions
-
-
-# In[287]:
-
-
-def new_sentence(utterance):
-    """
-    Rule-based prediction of a dialog act based on a phrase.
-    Arguments:
-        utterance: string
-        
-    Returns:
-        Returns the predicted dialog act.
-    """
-    utterance = utterance.lower()
-    if search(r'\bhow about\b|\bwhat about\b|\banything else\b|\bare there\b|\bis there\b|\bwhat else\b', utterance):
-        return 'reqalts'
-    elif search(r'\byes\b|\byeah\b|\bcorrect\b',utterance):
-        return 'affirm'
-    elif search(r'\bthank you\b', utterance):
-        return 'thankyou'
-    elif search(r'\bgoodbye\b|\bbye\b', utterance):
-        return 'bye'
-    elif search(r'\bdoes it\b|\bis it\b|\bdo they\b|\bis that\b|\bis there\b', utterance):
-        return 'confirm'
-    elif search(r'\bwhat is\b|\bwhats\b|\bmay i\b|\bcould i\b|\bwhat\b|\bprice range\b|\bpost code\b|\btype of\b|\baddress\b|\bphone number\b|\bcan i\b|\bcould i\b|\bcould you\b|\bdo you\b|\bi want+.address\b|\bi want+.phone\b|\bi would\b|\bwhere is\b', utterance):
-        return 'request'
-    elif search(r'\bno\b|\bnot\b', utterance):
-        return 'negate'
-    elif search(r'\blooking for\b|\bdont care\b|\bdoesnt matter\b|\bexpensive\b|\bcheap\b|\bmoderate\b|\bi need\b|\bi want\b|\bfood\b|\bnorth\b',utterance):
-        return 'inform'
-    elif search(r'\bdont\b', utterance):
-        return 'deny'
-    elif search(r'\bhello\b|\bhi\b|\bhey\b', utterance):
-        return 'hello'
-    elif search(r'\brepeat\b', utterance):
-        return 'repeat'
-    elif search(r'\bmore\b', utterance):
-        return 'reqmore'
-    elif search(r'\bstart\b', utterance):
-        return 'restart'
-    elif search(r'\bokay\b|\bkay\b',utterance):
-        return 'ack'
-    else:
-        return 'inform'
-    
-
-
 # In[288]:
 
 
-def plot_confusion_matrix(labels,predictions):
-    """Plots the confusion matrix
-    Arguments:
-    labels: array-like of shape (n_samples,)
-    predictions: array-like of shape (n_samples,)
-    Returns
-    -------
-    plot
-        plots the confusion matrix
-    """
-    plt.rcParams.update(plt.rcParamsDefault)
 
-    plt.rcParams['figure.figsize'] = [10, 10]
-    plt.rcParams['font.size'] = 8
-    sklearn.metrics.ConfusionMatrixDisplay.from_predictions(labels,predictions)
-
-    plt.show()
 
 
 # In[289]:
 
 
-predictions = (rules(df))
+
 
 
 # In[290]:
 
 
-def baselineAccuracy(predictions, df):
-    """Calculates the accuracy
-        Arguments:
-        predictions: list
-        df: a pandas dataframe that contains a column named text with utterances.
-        Returns
-        -------
-    Returns:
-        Returns the accuracy
-    """
-    count = 0
-    for i in range(0,len(predictions)):
-        
-        if(predictions[i].lower() == df.loc[i,'label'].lower()):
-            count += 1
-    return "Accuracy: "+str(round(count / len(predictions)*100,1))+"%"
-        
+
 
 
 # In[291]:
 
 
-baselineAccuracy(predictions, df)
+
 
 
 # In[292]:
 
 
-plot_confusion_matrix(df['label'],predictions)
+
 
 
 # In[293]:
 
 
-def metrics_overview(labels, predictions):
-       """Prints metrics
-       Arguments:
-       labels: array-like of shape (n_samples,)
-       predictions: array-like of shape (n_samples,)
-       
-       Prints different metrics related to the confusion matrix.
-       """
-       edges_confusion_matrix = sklearn.metrics.confusion_matrix(labels,predictions)
 
-       FP = edges_confusion_matrix.sum(axis=0) - np.diag(edges_confusion_matrix)  
-       
-       FN = edges_confusion_matrix.sum(axis=1) - np.diag(edges_confusion_matrix)
-       
-       TP = np.diag(edges_confusion_matrix)
-       
-       TN = edges_confusion_matrix.sum() - (FP + FN + TP)
-       
-       
-       # Sensitivity, hit rate, recall, or true positive rate
-       TPR = TP/(TP+FN)
-       print('TPR',TPR)
-       print('Average TPR',np.average(TPR))
-       print('_______________________________')
-       # Specificity or true negative rate
-       TNR = TN/(TN+FP)
-       print('TNR',TNR)
-       print('Average TNR',np.average(TNR))
-       print('_______________________________')
-
-       # Precision or positive predictive value
-       PPV = TP/(TP+FP)
-       print('PPV',PPV)
-       print('Average PPV',np.average(PPV))
-       print('_______________________________')
-
-       # Negative predictive value
-       NPV = TN/(TN+FN)
-       print('NPV',NPV)
-       print('Average NPV',np.average(NPV))
-       print('_______________________________')
-
-       # Fall out or false positive rate
-       FPR = FP/(FP+TN)
-       print('FPR',FPR)
-       print('Average FPR',np.average(FPR))
-       print('_______________________________')
-
-       # False negative rate
-       FNR = FN/(TP+FN)
-       print('FNR',FNR)
-       print('Average FNR',np.average(FNR))
-       print('_______________________________')
-
-       # False discovery rate
-       FDR = FP/(TP+FP)
-       print('FDR',FDR)
-       print('Average FDR',np.average(FDR))
-       print('_______________________________')
-
-       # Overall accuracy
-       ACC = (TP+TN)/(TP+FP+FN+TN)
-       print('ACC',ACC)
-       print('Average ACC',np.average(ACC))
-       print('_______________________________')
-
-       F1 = 2*((PPV*TPR)/(PPV+TPR))
-       F1 = F1[~np.isnan(F1)]
-       print('F1',F1)
-       print('Average F1',np.average(F1))
-       print('_______________________________')
-       print((FP+FN)/(TP+FP+FN+TN))
 
 
 # In[294]:
 
 
-metrics_overview(df['label'],predictions)
+
 
--- main_notebook.py
+++ main_notebook.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Text Classification Assignment
+# # WIP - Restaurant Recommendation Dialog System
+# 
+# ## Members:
+# - Karpiński, R.R. (Rafał)
+# - Pavan, L. (Lorenzo)
+# - Rodrigues Luchetti, G.L. (Gustavo)
+# - Teunissen, N.D. (Niels)
 
 # In[1]:
 
@@ -14,7 +20,7 @@ import sklearn.metrics
 from re import search
 
 
-# ## Reading 'dialog_acts.dat' Dataset
+# ## Reading 'dialog_acts.dat' into dataframe
 
 # In[2]:
 
@@ -36,7 +42,7 @@ df.head(3)
 # 
 # Looking for null values, irrelevant or noisy text (literally, removing 'tv_noise' and 'noise') and repeated values. Formatting labels into numbers.
 
-# In[18]:
+# In[4]:
 
 
 df['label_id'] = df['label'].factorize()[0]
@@ -45,7 +51,7 @@ label_dict = df[['label','label_id']].drop_duplicates().set_index('label_id')
 label_dict
 
 
-# In[8]:
+# In[5]:
 
 
 from sklearn.model_selection import train_test_split
@@ -57,7 +63,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 
-# In[16]:
+# In[13]:
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -69,16 +75,111 @@ tfidf = TfidfVectorizer(sublinear_tf=True, # scale the words frequency in logari
                         lowercase=True # everything to lowercase
                         )
 
-features = tfidf.fit_transform(text_train).toarray()
-targets = label_train
+X_train_tfid = tfidf.fit_transform(X_train).toarray()
+X_test_tfid = tfidf.fit_transform(X_test).toarray()
 
-print(features)
-print(targets)
+print(X_train_tfid)
+print(y_train)
 
 
-# ## Models
+# ## Building Baseline Systems
 
-# In[ ]:
+# In[7]:
+
+
+def BaselineRuleBased(dataframe):
+    """
+    Rule-based prediction of dialog acts based on utterances.
+    Arguments:
+        dataframe: a pandas dataframe that contains a column named text with utterances.
+        
+    Returns:
+        Returns a list of predictions about the label (dialog act) of the utterances.
+    """
+    predictions = []
+    for i in range(0,len(dataframe)):
+        if search(r'\bhow about\b|\bwhat about\b|\banything else\b|\bare there\b|\bis there\b|\bwhat else\b', df.loc[i,'text']):
+            predictions.append('reqalts')
+        elif search(r'\byes\b|\byeah\b|\bcorrect\b',df.loc[i,'text']):
+            predictions.append('affirm')
+        elif search(r'\bthank you\b', df.loc[i,'text']):
+            predictions.append('thankyou')
+        elif search(r'\bgoodbye\b|\bbye\b', df.loc[i,'text']):
+            predictions.append('bye')
+        elif search(r'\bdoes it\b|\bis it\b|\bdo they\b|\bis that\b|\bis there\b', df.loc[i,'text']):
+            predictions.append('confirm')
+        elif search(r'\bwhat is\b|\bwhats\b|\bmay i\b|\bcould i\b|\bwhat\b|\bprice range\b|\bpost code\b|\btype of\b|\baddress\b|\bphone number\b|\bcan i\b|\bcould i\b|\bcould you\b|\bdo you\b|\bi want+.address\b|\bi want+.phone\b|\bi would\b|\bwhere is\b', df.loc[i,'text']):
+            predictions.append('request')
+        elif search(r'\bno\b|\bnot\b', df.loc[i,'text']):
+            predictions.append('negate')
+        elif search(r'\blooking for\b|\bdont care\b|\bdoesnt matter\b|\bexpensive\b|\bcheap\b|\bmoderate\b|\bi need\b|\bi want\b|\bfood\b|\bnorth\b',df.loc[i,'text']):
+            predictions.append('inform')
+        elif search(r'\bdont\b', df.loc[i,'text']):
+            predictions.append('deny')
+        elif search(r'\bhello\b', df.loc[i,'text']):
+            predictions.append('hello')
+        elif search(r'\brepeat\b', df.loc[i,'text']):
+            predictions.append('repeat')
+        elif search(r'\bmore\b', df.loc[i,'text']):
+            predictions.append('reqmore')
+        elif search(r'\bstart\b', df.loc[i,'text']):
+            predictions.append('restart')
+        elif search(r'\bokay\b|\bkay\b',df.loc[i,'text']):
+            predictions.append('ack')
+        else:
+            predictions.append('inform')
+    return predictions
+
+
+# In[8]:
+
+
+def new_sentence(utterance):
+    """
+    Rule-based prediction of a dialog act based on a phrase.
+    Arguments:
+        utterance: string
+        
+    Returns:
+        Returns the predicted dialog act.
+    """
+    utterance = utterance.lower()
+    if search(r'\bhow about\b|\bwhat about\b|\banything else\b|\bare there\b|\bis there\b|\bwhat else\b', utterance):
+        return 'reqalts'
+    elif search(r'\byes\b|\byeah\b|\bcorrect\b',utterance):
+        return 'affirm'
+    elif search(r'\bthank you\b', utterance):
+        return 'thankyou'
+    elif search(r'\bgoodbye\b|\bbye\b', utterance):
+        return 'bye'
+    elif search(r'\bdoes it\b|\bis it\b|\bdo they\b|\bis that\b|\bis there\b', utterance):
+        return 'confirm'
+    elif search(r'\bwhat is\b|\bwhats\b|\bmay i\b|\bcould i\b|\bwhat\b|\bprice range\b|\bpost code\b|\btype of\b|\baddress\b|\bphone number\b|\bcan i\b|\bcould i\b|\bcould you\b|\bdo you\b|\bi want+.address\b|\bi want+.phone\b|\bi would\b|\bwhere is\b', utterance):
+        return 'request'
+    elif search(r'\bno\b|\bnot\b', utterance):
+        return 'negate'
+    elif search(r'\blooking for\b|\bdont care\b|\bdoesnt matter\b|\bexpensive\b|\bcheap\b|\bmoderate\b|\bi need\b|\bi want\b|\bfood\b|\bnorth\b',utterance):
+        return 'inform'
+    elif search(r'\bdont\b', utterance):
+        return 'deny'
+    elif search(r'\bhello\b|\bhi\b|\bhey\b', utterance):
+        return 'hello'
+    elif search(r'\brepeat\b', utterance):
+        return 'repeat'
+    elif search(r'\bmore\b', utterance):
+        return 'reqmore'
+    elif search(r'\bstart\b', utterance):
+        return 'restart'
+    elif search(r'\bokay\b|\bkay\b',utterance):
+        return 'ack'
+    else:
+        return 'inform'
+    
+
+
+# ## Building Classifier Models
+
+# In[9]:
 
 
 from sklearn.naive_bayes import MultinomialNB
@@ -92,7 +193,7 @@ models = [
 ]
 
 
-# In[ ]:
+# In[11]:
 
 
 from sklearn.model_selection import cross_val_score
@@ -103,13 +204,13 @@ cv_df = pd.DataFrame(index=range(CV * len(models)))
 
 for model in models:
   model_name = model.__class__.__name__
-  accuracies = cross_val_score(model, features, labels, scoring='accuracy')
+  accuracies = cross_val_score(model, X_train_tfid, y_train, scoring='accuracy')
   for fold_idx, accuracy in enumerate(accuracies):
     entries.append((model_name, fold_idx, accuracy))
 cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
 
 
-# In[ ]:
+# In[12]:
 
 
 mean_accuracy = cv_df.groupby('model_name').accuracy.mean()
@@ -119,42 +220,161 @@ acc = pd.concat([mean_accuracy, std_accuracy], axis= 1,
           ignore_index=True)
 acc.columns = ['Mean Accuracy', 'Standard deviation']
 
-acc # seems like LogisticRegression is the best or we can tweek the hyperparameters more. 
+acc 
 
 
-# In[ ]:
+# seems like LogisticRegression is the best or we can tweek the hyperparameters more. 
 
-
-features_test = tfidf.transform(text_test).toarray()
-
-
-# In[ ]:
+# In[15]:
 
 
 model = LogisticRegression(random_state=0, max_iter=400)
-model.fit(features, labels)
-label_pred = model.predict(features_test)
-
-
-# In[ ]:
-
-
-
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
 
 # ## Evaluations
 
 # ### Baseline Systems
 
+# In[16]:
+
+
+def plot_confusion_matrix(labels,predictions):
+    """Plots the confusion matrix
+    Arguments:
+    labels: array-like of shape (n_samples,)
+    predictions: array-like of shape (n_samples,)
+    Returns
+    -------
+    plot
+        plots the confusion matrix
+    """
+    plt.rcParams.update(plt.rcParamsDefault)
+
+    plt.rcParams['figure.figsize'] = [10, 10]
+    plt.rcParams['font.size'] = 8
+    sklearn.metrics.ConfusionMatrixDisplay.from_predictions(labels,predictions)
+
+    plt.show()
+
+
+# In[17]:
+
+
+predictions = (rules(df))
+def baselineAccuracy(predictions, df):
+    """Calculates the accuracy
+        Arguments:
+        predictions: list
+        df: a pandas dataframe that contains a column named text with utterances.
+        Returns
+        -------
+    Returns:
+        Returns the accuracy
+    """
+    count = 0
+    for i in range(0,len(predictions)):
+        
+        if(predictions[i].lower() == df.loc[i,'label'].lower()):
+            count += 1
+    return "Accuracy: "+str(round(count / len(predictions)*100,1))+"%"
+
+
 # In[ ]:
 
 
+baselineAccuracy(predictions, df)
 
+
+# In[ ]:
+
+
+plot_confusion_matrix(df['label'],predictions)
+
+
+# In[ ]:
+
+
+def metrics_overview(labels, predictions):
+       """Prints metrics
+       Arguments:
+       labels: array-like of shape (n_samples,)
+       predictions: array-like of shape (n_samples,)
+       
+       Prints different metrics related to the confusion matrix.
+       """
+       edges_confusion_matrix = sklearn.metrics.confusion_matrix(labels,predictions)
+
+       FP = edges_confusion_matrix.sum(axis=0) - np.diag(edges_confusion_matrix)  
+       
+       FN = edges_confusion_matrix.sum(axis=1) - np.diag(edges_confusion_matrix)
+       
+       TP = np.diag(edges_confusion_matrix)
+       
+       TN = edges_confusion_matrix.sum() - (FP + FN + TP)
+       
+       
+       # Sensitivity, hit rate, recall, or true positive rate
+       TPR = TP/(TP+FN)
+       print('TPR',TPR)
+       print('Average TPR',np.average(TPR))
+       print('_______________________________')
+       # Specificity or true negative rate
+       TNR = TN/(TN+FP)
+       print('TNR',TNR)
+       print('Average TNR',np.average(TNR))
+       print('_______________________________')
+
+       # Precision or positive predictive value
+       PPV = TP/(TP+FP)
+       print('PPV',PPV)
+       print('Average PPV',np.average(PPV))
+       print('_______________________________')
+
+       # Negative predictive value
+       NPV = TN/(TN+FN)
+       print('NPV',NPV)
+       print('Average NPV',np.average(NPV))
+       print('_______________________________')
+
+       # Fall out or false positive rate
+       FPR = FP/(FP+TN)
+       print('FPR',FPR)
+       print('Average FPR',np.average(FPR))
+       print('_______________________________')
+
+       # False negative rate
+       FNR = FN/(TP+FN)
+       print('FNR',FNR)
+       print('Average FNR',np.average(FNR))
+       print('_______________________________')
+
+       # False discovery rate
+       FDR = FP/(TP+FP)
+       print('FDR',FDR)
+       print('Average FDR',np.average(FDR))
+       print('_______________________________')
+
+       # Overall accuracy
+       ACC = (TP+TN)/(TP+FP+FN+TN)
+       print('ACC',ACC)
+       print('Average ACC',np.average(ACC))
+       print('_______________________________')
+
+       F1 = 2*((PPV*TPR)/(PPV+TPR))
+       F1 = F1[~np.isnan(F1)]
+       print('F1',F1)
+       print('Average F1',np.average(F1))
+       print('_______________________________')
+       print((FP+FN)/(TP+FP+FN+TN))
+       
+metrics_overview(df['label'],predictions)
 
 
 # ### Proper Models (Random Forest, Multinomial NB, Logistic Regression)
 
-# In[17]:
+# In[ ]:
 
 
 from sklearn.metrics import confusion_matrix
