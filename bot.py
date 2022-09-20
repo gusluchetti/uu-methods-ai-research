562c864fe74a3cba4ddaa5a775fd135a4509a9a2
Gustavo Luchetti <gustavo@luchetti.dev>
2022-09-20 12:53:11 +0200

    feat: big update to code logic

--- bot.py
+++ bot.py
@@ -1,31 +1,32 @@
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
+# passing functions that return predictions for the bot to use
+def bot(list_models):
+    print("Hello! I'm a restaurant recommendation bot! \n")
+    classifier_key = input("""
+    Please select a classifier method:
+    (1-Default) - 1st Baseline - Majority Class: Always predicts the majority class (in this case, 'inform' label)
+    (2) - 2nd Baseline - Keyword Matching: Predictions are based on keywords found in the utterance
+    (3) - Multinomial Naive-Bayes
+    (4) - Logistic Regression
+    """)
 
-# bot setup
-def bot(classifier):
-    list_classifiers = {
-        1: "majority class baseline",
-        2: "keyword based baseline",
-        3: "multinominal nb",
-        4: "logistic regression"
-    }
+    # set desired classifier
+    classifier = list_models["1"]
+    try:
+        if classifier_key in list_models.keys():
+            classifier = list_models[classifier_key]
+            print(f"Model {classifier} was selected!")
+        else:
+            print('Model doesnt exist!')
+    except:
+        print('whoops! - bad things happened somehow')
 
+    # starting bot up
+    finished = False
     while not finished:
-        classifier_key = input("""
-            Select your classifier:
-            (1) - 1st Baseline - Majority Class: Always predicts the majority class (in this case, 'inform' label)
-            (2) - 2nd Baseline - Keyword Matching: Predictions are based on keywords found in the utterance
-            (3) - Multinomial Naive-Bayes
-            (4) - Logistic Regression
-        """)
+        utterance = input('>').lower()
+        classification = classifier(utterance)
+        print(f"Your last input was labeled/classified as : {classification}")
 
-        try:
-            if classifier_key in list_classifiers:
-                classifier = list_classifiers[classifier_key]
-            else:
-                print('Model doesnt exist!')
-        except:
-            print('whoops!')
-
-        text = input('>').lower()
-
+        if utterance == "forcequit" or classification == "bye":
+            finished = True
+            print("Bye! See ya later.")
