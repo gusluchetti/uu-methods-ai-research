895ecda51b4d91c63aa6f136fcafb66b9844c0ce
Gustavo Luchetti <gustavo@luchetti.dev>
2022-10-08 02:40:11 +0200

    wip: major refactor
    instead of single huge function, separate into smaller functions for
    each form state extraction, as to enable all of them to have "any" value

--- type_match_ls.py
+++ type_match_ls.py
@@ -4,12 +4,9 @@ from Levenshtein import distance
 log = logging.getLogger(__name__)
 
 
-# IMPORTANT TODO:
-# populate list of price ranges, locations and food types based on
-# restaurant dataframe uniques (for each of the columns?)
 def best_word_match(word_to_match, wordtype_l):
     """
-    takes misspelled word and matches it with nearest
+    Takes misspelled word and matches it with nearest
     value of the list of words of the same type.
 
     args: word_to_match (str) -> the misspelled word
@@ -53,12 +50,106 @@ def best_match_list(list_of_words, wordtype_list):
         return bestmatch, misspelled
 
 
-def extract_preference(user_utt):
-    """
-    args : user_utt - user
-    return : dictionary of fields corresponding to global preference_form
-        filled with a (keyword | "any" | "")
-    """
+def pre_process(user_utt):
+    return list(user_utt.lower().split(" "))
+
+
+form = {
+    "area": "",
+    "pricerange": "",
+    "food": "",
+    "extra_preference": ""
+}
+location_index = -1
+pricerange_index = -1
+type_index = -1
+
+
+def extract_area(user_utt):
+    user_utt = pre_process(user_utt)
+    global form, location_index
+    location = ""
+    locations = [
+        "any",
+        "north",
+        "east",
+        "west",
+        "south",
+        "center"
+    ]
+    location_index = -3
+    for local in locations:
+        if local in user_utt:
+            location_index = user_utt.index(local)
+            location = local
+
+    # If no keyword was matched, look at positions in user_utt where you would
+    # expect to find a location. If no keyword relative to another category (type
+    # or pricerange) was found at that position, choose the word from that position
+    # which has the lowest Levenshtein edit distance from our keywords
+    if location == "":
+        location_candidates = []
+        for word in user_utt:
+            if (
+                (word == "area" or word == "part")
+                and (user_utt.index(word) != 0)
+                and (user_utt.index(word) - 1 != type_index)
+                and (user_utt.index(word) - 1 != pricerange_index)
+            ):
+                location_candidates.append(user_utt[user_utt.index(word) - 1])
+        if best_match_list(location_candidates, locations):
+            location, misspelled_location = best_match_list(
+                location_candidates, locations
+            )
+            location_index = user_utt.index(misspelled_location)
+
+    log.debug(f"got {location} location")
+
+    form["area"] = location
+    return form
+
+
+def extract_pricerange(user_utt):
+    user_utt = pre_process(user_utt)
+    global form, pricerange_index
+    pricerange = ""
+    # find the index of the word on pricerange
+    price_ranges = [
+        "any",
+        "cheap",
+        "moderate",
+        "expensive"
+    ]
+    pricerange_index = -1
+
+    for price in price_ranges:
+        if price in user_utt:
+            pricerange_index = user_utt.index(price)
+            pricerange = price
+
+    # If no keyword was matched, look at positions in sentence where you would
+    # expect to find a price. Choose the word from that position
+    # which has the lowest Levenshtein edit distance from our keywords
+    if pricerange == "":
+        pricerange_candidates = []
+        for word in user_utt:
+            if (word == "priced") and (user_utt.index(word) != 0):
+                pricerange_candidates.append(user_utt[user_utt.index(word) - 1])
+        if best_match_list(pricerange_candidates, ["moderate", "cheap", "expensive"]):
+            pricerange, misspelled_pricerange = best_match_list(
+                pricerange_candidates, ["moderate", "cheap", "expensive"]
+            )
+            pricerange_index = user_utt.index(misspelled_pricerange)
+
+    log.debug(f"got {pricerange} price range")
+
+    form["pricerange"] = pricerange
+    return form
+
+
+def extract_food(user_utt):
+    user_utt = pre_process(user_utt)
+    global form, type_index
     food_types = [
         "african",
         "any",
@@ -99,6 +190,42 @@ def extract_preference(user_utt):
         "vietnamese",
     ]
 
+    type_of_food = ""
+    # find the index of the word on type of food.
+    # make sure it is NOT the same as the one for pricerange
+    type_index = -2
+    type_candidates = []
+    for i, word in enumerate(user_utt):
+        if word in food_types and word != "any":
+            type_of_food = word
+            type_index = user_utt.index(word)
+            break
+        if (
+            (word == "food" or word == "restaurant")
+            and (user_utt.index(word) != 0)
+            and (user_utt.index(word) - 1 != pricerange_index)
+        ):
+            type_candidates.append(user_utt[user_utt.index(word) - 1])
+        elif (
+            (word == "serving" or word == "serves")
+            and (user_utt.index(word) != user_utt.index(user_utt[-1]))
+            and (user_utt.index(word) + 1 != pricerange_index)
+        ):
+            type_candidates.append(user_utt[user_utt.index(word) + 1])
+    if best_match_list(type_candidates, food_types):
+        type_of_food, misspelled_type = best_match_list(type_candidates, food_types)
+        type_index = user_utt.index(misspelled_type)
+
+    log.debug(f"got {type_of_food} food type")
+
+    form["food"] = type_of_food
+    return form
+
+
+def extract_extra_preference(user_utt):
+    user_utt = pre_process(user_utt)
+    global form, location_index, pricerange_index, type_index
+    extra_preference = ""
     extra_pref_list = [
         "touristic",
         "children",
@@ -106,162 +233,17 @@ def extract_preference(user_utt):
         "romantic",
         "seats assigned",
     ]
-
-    # preprocessing
-    sentence_string = user_utt
-    sentence_string = sentence_string.lower()
-    sentence_string = sentence_string.replace("north american", "northamerican")
-    sentence_string = sentence_string.replace("modern european", "moderneuropean")
-    sentence_string = sentence_string.replace("asian oriental", "asianoriental")
-    sentence_string = sentence_string.replace("assigned seats", "assignedseats")
-    sentence_string = sentence_string.replace("seats assigned", "seatsassigned")
-    sentence = list(sentence_string.split(" "))
-    sentence = [
-        "north american" if item == "northamerican" else item for item in sentence
-    ]
-    sentence = [
-        "modern european" if item == "moderneuropean" else item for item in sentence
-    ]
-    sentence = [
-        "asian oriental" if item == "asianoriental" else item for item in sentence
-    ]
-    sentence = [
-        "seats assigned" if item == "seatsassigned" else item for item in sentence
-    ]
-    sentence = [
-        "assigned seats" if item == "assignedseats" else item for item in sentence
-    ]
-
-    type_of_food = ""
-    location = ""
-    pricerange = ""
-    extra_preference = ""
-
-    # find the index of the word on pricerange
-    price_ranges = [
-        "any",
-        "cheap",
-        "moderate",
-        "expensive"
-    ]
-    pricerange_index = -1
-
-    for price in price_ranges:
-        if price in sentence:
-            pricerange_index = sentence.index(price)
-            pricerange = price
-
-    # If no keyword was matched, look at positions in sentence where you would
-    # expect to find a price. Choose the word from that position
-    # which has the lowest Levenshtein edit distance from our keywords
-    if pricerange == "":
-        pricerange_candidates = []
-        for word in sentence:
-            if (word == "priced") and (sentence.index(word) != 0):
-                pricerange_candidates.append(sentence[sentence.index(word) - 1])
-        if best_match_list(pricerange_candidates, ["moderate", "cheap", "expensive"]):
-            pricerange, misspelled_pricerange = best_match_list(
-                pricerange_candidates, ["moderate", "cheap", "expensive"]
-            )
-            pricerange_index = sentence.index(misspelled_pricerange)
-
-    log.debug(f"got {pricerange} price range")
-
-    # find the index of the word on type of food.
-    # make sure it is NOT the same as the one for pricerange
-    type_index = -2
-    type_candidates = []
-    for i, word in enumerate(sentence):
-        if word in food_types and word != "any":
-            type_of_food = word
-            type_index = sentence.index(word)
-            break
-        if (
-            (word == "food" or word == "restaurant")
-            and (sentence.index(word) != 0)
-            and (sentence.index(word) - 1 != pricerange_index)
-        ):
-            type_candidates.append(sentence[sentence.index(word) - 1])
-        elif (
-            (word == "serving" or word == "serves")
-            and (sentence.index(word) != sentence.index(sentence[-1]))
-            and (sentence.index(word) + 1 != pricerange_index)
-        ):
-            type_candidates.append(sentence[sentence.index(word) + 1])
-    if best_match_list(type_candidates, food_types):
-        type_of_food, misspelled_type = best_match_list(type_candidates, food_types)
-        type_index = sentence.index(misspelled_type)
-
-    log.debug(f"got {type_of_food} food type")
-
-    # find the index of the word on location.
-    locations = [
-        "any",
-        "north",
-        "east",
-        "west",
-        "south",
-        "center"
-    ]
-    location_index = -3
-    for local in locations:
-        if local in sentence:
-            location_index = sentence.index(local)
-            location = local
-
-    # If no keyword was matched, look at positions in sentence where you would
-    # expect to find a location. If no keyword relative to another category (type
-    # or pricerange) was found at that position, choose the word from that position
-    # which has the lowest Levenshtein edit distance from our keywords
-    if location == "":
-        location_candidates = []
-        for word in sentence:
-            if (
-                (word == "area" or word == "part")
-                and (sentence.index(word) != 0)
-                and (sentence.index(word) - 1 != type_index)
-                and (sentence.index(word) - 1 != pricerange_index)
-            ):
-                location_candidates.append(sentence[sentence.index(word) - 1])
-        if best_match_list(location_candidates, locations):
-            location, misspelled_location = best_match_list(
-                location_candidates, locations
-            )
-            location_index = sentence.index(misspelled_location)
-
-    log.debug(f"got {location} location")
-
     # find extra preferences, if the user has any
-    if best_match_list(sentence, extra_pref_list):
+    if best_match_list(user_utt, extra_pref_list):
         extra_preference, misspelled_extra_pref = best_match_list(
-            sentence, extra_pref_list
+            user_utt, extra_pref_list
         )
         if (
-            sentence.index(misspelled_extra_pref) == location_index
-            or sentence.index(misspelled_extra_pref) == pricerange_index
-            or sentence.index(misspelled_extra_pref) == type_index
+            user_utt.index(misspelled_extra_pref) == location_index
+            or user_utt.index(misspelled_extra_pref) == pricerange_index
+            or user_utt.index(misspelled_extra_pref) == type_index
         ):
             extra_preference = "unknown"
 
-    return {
-        "area": location,
-        "pricerange": pricerange,
-        "food": type_of_food,
-        "extra_preference": extra_preference,
-    }
-
-
-def extract_food(user_utt):
-    return extract_preference(user_utt)["food"]
-
-
-def extract_area(user_utt):
-    return extract_preference(user_utt)["area"]
-
-
-def extract_pricerange(user_utt):
-    return extract_preference(user_utt)["pricerange"]
-
-
-def extract_extra_preference(user_utt):
-    return extract_preference(user_utt)["extra_preference"]
+    form["extra_preference"] = extra_preference
+    return form
