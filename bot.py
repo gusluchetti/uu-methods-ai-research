7ec5b866028fbceb277afacf70626ccd2f00ad3c
Gustavo Luchetti <43828310+gusluchetti@users.noreply.github.com>
2022-10-10 05:06:56 +0200

    Feat: Adding more configuration options (#30)
    * feat: commenting more options not set yet
    
    * feat: uncommenting some options
    
    * feat: dynamic show option menu
    
    * feat: update model selection to also use pick
    
    * format: code formatting
    
    * format: add utility functions to lib file
    
    * feat: change model order
    
    * feat: list models is actually a list
    
    * fix: global classifier in bot py
    
    * feat: replace functions acessing dicts
    
    * feat: function for confirm msg
    
    * feat: update comment
    
    * feat: remove get_form usage
    
    * fix: fix confirm msg logic
    
    * fix: add new arguments for confirm msg
    
    * feat: enable loud setting
    
    * feat: add delayed config option
    
    * feat: update TODO for settings
    
    * feat: refactor enable settings
    
    * feat: remove print
    
    * feat: add todo

--- bot.py
+++ bot.py
@@ -1,150 +1,12 @@
 import logging
-from pick import pick
+import time
 
 # local imports
-import type_match_ls
+import type_match_ls as ls
 import restaurant
+import lib
 
 log = logging.getLogger(__name__)
-
-
-def create_settings_dict():
-    # TODO:
-    # leven_edit - edit levenshtein distance for preference extraction, should be a different menu maybe?
-    # fancy_bot - does fancy bot mean the bot accepts fancy phrases from the user? or that the bot is fancier?
-    settings_dict = [
-        # {
-        #     "key": "confirm_leven",
-        #     "description": "Enable confirmation of correctness for Levenshtein distance matches",
-        #     "is_enabled": False
-        # },
-        # {
-        #     "key": "random_order",
-        #     "description": "Enable preferences to be stated in random order",
-        #     "is_enabled": False
-        # },
-        # {
-        #     "key": "stupid_bot",
-        #     "description": "Insert artificial errors in preference extraction",
-        #     "is_enabled": False
-        # },
-        {
-            "key": "enable_restart",
-            "description": "Enable being able to restart the dialogue at any moment",
-            "is_enabled": False
-        },
-        {
-            "key": "delayed",
-            "description": "Introduce a delay before showing system responses",
-            "is_enabled": False
-        },
-        {
-            "key": "thorough",
-            "description": "Enable confirmation for each preference",
-            "is_enabled": False
-        },
-        {
-            "key": "loud",
-            "description": "OUTPUT IN ALL CAPS!!",
-            "is_enabled": False
-        },
-        {
-            "key": "voice_assistant",
-            "description": "Enable text-to-speech for system utterances",
-            "is_enabled": False
-        }
-    ]
-    return settings_dict
-
-
-# TODO: add optional sys_dialog for failing conditions
-# we should alawys get out of the welcome node right?
-dialog_tree = {
-    "welcome": {
-        "mode": "welcome",
-        "sys_utt": "Hello! What kind of restaurant are you looking for?\n",
-        "exits": ["test_food"],
-        "exit_conditions": ["True"],
-    },
-    "test_food": {
-        "mode": "test",
-        "sys_utt": "",
-        "exits": ["ask_food", "test_area"],
-        "exit_conditions": ['not get_form("food")', "True"],
-    },
-    "ask_food": {
-        "mode": "extract_food",
-        "sys_utt": "What type of food would you like to eat?\n",
-        "exits": ["ask_food", "test_food"],
-        "exit_conditions": ['not get_form("food")', "True"],
-    },
-    "test_area": {
-        "mode": "test",
-        "sys_utt": "",
-        "exits": ["ask_area", "test_pricerange"],
-        "exit_conditions": ['not get_form("area")', "True"],
-    },
-    "ask_area": {
-        "mode": "extract_area",
-        "sys_utt": "Where would you like to eat?\n",
-        "exits": ["ask_area", "test_area"],
-        "exit_conditions": ['not get_form("area")', "True"],
-    },
-    "test_pricerange": {
-        "mode": "test",
-        "sys_utt": "",
-        "exits": ["ask_pricerange", "ask_extra_preference"],
-        "exit_conditions": ['not get_form("pricerange")', "True"],
-    },
-    "ask_pricerange": {
-        "mode": "extract_pricerange",
-        "sys_utt": "How pricy you want the food to be?\n",
-        "exits": ["ask_pricerange", "test_pricerange"],
-        "exit_conditions": ['not get_form("pricerange")', "True"],
-    },
-    "ask_extra_preference": {
-        "mode": "extract_extra_preference",
-        "sys_utt": "Do you have any extra preference?\n",
-        "exits": ["confirm_choice", "ask_extra_preference"],
-        "exit_conditions": [
-            "label in ['negate','deny'] or get_form('extra_preference')",
-            "True",
-        ],
-    },
-    "confirm_choice": {
-        "mode": "confirm",
-        "sys_utt": "",
-        "exits": ["welcome", "suggest_restaurant"],
-        "exit_conditions": ["label in ['negate','deny']", "True"],
-    },
-    "suggest_restaurant": {
-        "mode": "suggest",
-        "sys_utt": "",
-        "exits": ["suggest_restaurant", "goodbye", "goodbye"],
-        "exit_conditions": [
-            "label in ['negate','deny']",
-            "restaurant.get_recommendations().empty",
-            "True",
-        ],
-    },
-}
-
-# add capability to exit at each point in conversation
-for value in dialog_tree.values():
-    if value["mode"] != "test":
-        value["exits"].insert(0, "goodbye")
-        value["exit_conditions"].insert(0, 'label=="bye"')
-
-
-# get, set and reset form state
-def get_form(field):
-    global form
-    return form[field]
-
-
-def set_form(field, input):
-    global form
-    form[field] = input
 
 
 def reset_form():
@@ -152,97 +14,64 @@ def reset_form():
     form = {field: "" for field in form}
 
 
-# starting states
-current_node = "welcome"
-form = {"pricerange": "", "area": "", "food": "", "extra_preference": ""}
-
-
-def extract_food(utt):
-    return type_match_ls.extract_food(utt)
-
-
-def extract_area(utt):
-    return type_match_ls.extract_area(utt)
-
-
-def extract_pricerange(utt):
-    return type_match_ls.extract_pricerange(utt)
-
-
-def extract_extra_preference(utt):
-    return type_match_ls.extract_extra_preference(utt)
-
-
-def set_current_node(new_node):
-    global current_node
-    current_node = new_node
-
-
-def show_settings_menu(settings_dict):
-    s_list = []
-    for s in settings_dict:
-        key, desc = s["key"], s["description"]
-        s_list.append(f"{key} - {desc}")
-
-    title = "Configure your desired settings: "
-    options = s_list
-    selected = pick(options, title, multiselect=True, min_selection_count=0)
-    return (selected)  # list of tuples
-
-
-def enable_settings(settings_dict, selected):
+def enable_settings(s_dict, selected):
+    global dialog_tree
     log.debug(f"Selected options: {selected}")
     for s in selected:
-        setting = settings_dict[s[1]]["key"]
-        if setting == "enable_restart":
-            for value in dialog_tree.values():
-                if value["mode"] != "test":
-                    value["exits"].insert(0, "welcome")
-                    value["exit_conditions"].insert(0, '"restart" in user_utt')
-        if setting == "loud":
-            # upper all sys utts
-            print('loud')
+        setting = list(s_dict)[s[1]]
+        s_dict[setting]["is_enabled"] = True
+
+    # any setting that modifies aspects of the state system
+    for k, v in dialog_tree.items():
+        if s_dict["loud"]["is_enabled"]:
+            # FIXME: this doesnt consider recommendations msgs
+            v["sys_utt"] = v["sys_utt"].upper()
+        if s_dict["enable_restart"]["is_enabled"] and v["mode"] != "test":
+            v["exits"].insert(0, "welcome")
+            v["exit_conditions"].insert(0, '"restart" in user_utt')
+
+    return s_dict
 
 
-# passing functions that return predictions for the bot to use
+# called whenever user response is necessary
+def get_user_input() -> str:
+    return "test"
+
+
+# global variables
+dialog_tree = None
+classifier = None
+current_node = "welcome"
+form = {"food": "", "pricerange": "", "area": "", "extra_preference": ""}
+
+
 def start(list_models):
-    global classifier
+    global classifier, dialog_tree, current_node, classifier, form
+    dialog_tree = lib.create_dialog_tree()
 
-    print("Starting bot...")
+    # showing configurability menu
+    settings_dict = lib.create_settings_dict()
+    selected_settings = lib.show_options_menu(settings_dict, "Configure your desired settings", True)
+    settings_dict = enable_settings(settings_dict, selected_settings)
+    # showing model selection menu
+    models_dict = lib.create_models_dict(list_models)
+    selected_method = lib.show_options_menu(models_dict, "Select your classification model")
+    classifier = lib.enable_method(models_dict, selected_method)
+    log.debug(classifier)
 
-    settings_dict = create_settings_dict()
-    selected = show_settings_menu(settings_dict)
-    enable_settings(settings_dict, selected)
-
-    classifier_key = input(
-        """
-Please select a classification method (first two are baseline systems):
-[1] - Majority Class
-[2 (Default)] Keyword Matching
-[3] - Logistic Regression
-[4] - Multinomial Naive-Bayes\n"""
-    )
-    classifier = list_models["2"]
-    if classifier_key in list_models.keys():
-        classifier = list_models[classifier_key]
-        print(f"Using model {classifier_key}")
-    else:
-        print("Using default model (keyword matching)")
-
-    global current_node
     while current_node != "goodbye":
+        # FIXME: should only run right before user input is required
+        if settings_dict["delayed"]["is_enabled"] is True:
+            time.sleep(0.3)
+
         mode = dialog_tree[current_node]["mode"]
         sys_utt = dialog_tree[current_node]["sys_utt"]
         exits = dialog_tree[current_node]["exits"]
         conditions = dialog_tree[current_node]["exit_conditions"]
+        log.debug(f"\nCurrent Node: {current_node}\nMode: {mode}")
+        log.debug(f"\nExits: {exits}\nConditions: {conditions}\nForm: {form}")
 
-        global form
-        """Traversing utterance to update form states"""
-        log.debug(
-            f"\nCurrent Node: {current_node}\nMode: {mode}\nExits: {exits}\nConditions: {conditions}\nForm: {form}"
-        )
-
-        mode_split = mode.split("_", 1)
+        mode_split = mode.split("_", maxsplit=1)
         if mode_split[0] in ["ask", "extract", "welcome"]:
             user_utt = input(sys_utt).lower()
             label = classifier(user_utt)
@@ -250,36 +79,16 @@ Please select a classification method (first two are baseline systems):
 
             if mode == "welcome":
                 reset_form()
-                set_form("food", extract_food(user_utt))
-                set_form("area", extract_area(user_utt))
-                set_form("pricerange", extract_pricerange(user_utt))
-                set_form("extra_preference", extract_extra_preference(user_utt))
+                form["food"] = ls.extract_food(user_utt)
+                form["area"] = ls.extract_area(user_utt)
+                form["pricerange"] = ls.extract_pricerange(user_utt)
+                form["extra_preference"] = ls.extract_extra_preference(user_utt)
             elif "extract" in mode:
-                field = eval(f"extract_{mode_split[1]}(user_utt)")
-                set_form(mode_split[1], field)
+                field = eval(f"ls.extract_{mode_split[1]}(user_utt)")
+                form[mode_split[1]] = field
 
         if mode == "confirm":
-            area = get_form("area")
-            food = get_form("food")
-            pricerange = get_form("pricerange")
-
-            start = "You want to eat in a restaurant"
-            end = ", did I get that right?\n"
-            extras = ""
-
-            if food != "any" or "":
-                extras += f" that has {food} cuisine"
-            if area != "any" or "":
-                extras += f" somewhere around the {area}"
-            if pricerange != "any" or "":
-                extras += f" in the {pricerange} price range"
-
-            none = "All right! A surprise it is. Is that ok?\n"
-            if food == "any" and area == "any" and pricerange == "any":
-                sys_utt = none
-            else:
-                sys_utt = f"{start}{extras}{end}"
-
+            sys_utt = lib.get_confirmation_msg(form)
             user_utt = input(sys_utt)
             label = classifier(user_utt)
             log.debug(f"classified utterance as {label}")
@@ -301,10 +110,12 @@ Please select a classification method (first two are baseline systems):
 
         # evaluate exit conditions
         for i, condition in enumerate(conditions):
-            log.debug(f"condition {i}: {condition} is evaluated as {eval(condition)}")
-            if eval(condition):
+            evaluation = eval(condition)
+            log.debug(f"condition {i}: {condition} is {evaluation}")
+
+            if evaluation:
                 next_node = exits[i]
                 break
 
-        set_current_node(next_node)
+        current_node = next_node
     print("Goodbye! Thanks for using our bot!")
---
+++ lib.py
@@ -0,0 +1,185 @@
+import logging
+from pick import pick
+
+log = logging.getLogger(__name__)
+# here lies dict creation and other string bulding operations used in bot.py
+
+
+def create_dialog_tree():
+    # TODO: add optional sys_dialog for failing conditions
+    # we should alawys get out of the welcome node right?
+    dialog_tree = {
+        "welcome": {
+            "mode": "welcome",
+            "sys_utt": "Hello! What kind of restaurant are you looking for?\n",
+            "exits": ["test_food"],
+            "exit_conditions": ["True"],
+        },
+        "test_food": {
+            "mode": "test",
+            "sys_utt": "",
+            "exits": ["ask_food", "test_area"],
+            "exit_conditions": ['not form["food"]', "True"],
+        },
+        "ask_food": {
+            "mode": "extract_food",
+            "sys_utt": "What type of food would you like to eat?\n",
+            "exits": ["ask_food", "test_food"],
+            "exit_conditions": ['not form["food"]', "True"],
+        },
+        "test_area": {
+            "mode": "test",
+            "sys_utt": "",
+            "exits": ["ask_area", "test_pricerange"],
+            "exit_conditions": ['not form["food"]', "True"],
+        },
+        "ask_area": {
+            "mode": "extract_area",
+            "sys_utt": "Where would you like to eat?\n",
+            "exits": ["ask_area", "test_area"],
+            "exit_conditions": ['not form["area"]', "True"],
+        },
+        "test_pricerange": {
+            "mode": "test",
+            "sys_utt": "",
+            "exits": ["ask_pricerange", "ask_extra_preference"],
+            "exit_conditions": ['not form["pricerange"]', "True"],
+        },
+        "ask_pricerange": {
+            "mode": "extract_pricerange",
+            "sys_utt": "How pricy you want the food to be?\n",
+            "exits": ["ask_pricerange", "test_pricerange"],
+            "exit_conditions": ['not form["pricerange"]', "True"],
+        },
+        "ask_extra_preference": {
+            "mode": "extract_extra_preference",
+            "sys_utt": "Do you have any extra preference?\n",
+            "exits": ["confirm_choice", "ask_extra_preference"],
+            "exit_conditions": [
+                "label in ['negate','deny'] or form['extra_preference']",
+                "True",
+            ],
+        },
+        "confirm_choice": {
+            "mode": "confirm",
+            "sys_utt": "",
+            "exits": ["welcome", "suggest_restaurant"],
+            "exit_conditions": ["label in ['negate','deny']", "True"],
+        },
+        "suggest_restaurant": {
+            "mode": "suggest",
+            "sys_utt": "",
+            "exits": ["suggest_restaurant", "goodbye", "goodbye"],
+            "exit_conditions": [
+                "label in ['negate','deny']",
+                "restaurant.get_recommendations().empty",
+                "True",
+            ],
+        }
+    }
+
+    # add capability to exit at each point in conversation
+    for value in dialog_tree.values():
+        if value["mode"] != "test":
+            value["exits"].insert(0, "goodbye")
+            value["exit_conditions"].insert(0, 'label=="bye"')
+
+    return dialog_tree
+
+
+def create_settings_dict():
+    # TODO: validate all these settings (try and implement the hardest!)
+    # leven_edit - edit levenshtein distance for preference extraction, should be a different menu maybe?
+    # confirm_leven - enable confirmation of correctness for levenshtein distance matches, what does that mean?
+    # fancy_bot - does fancy bot mean the bot accepts fancy phrases from the user? or that the bot is fancier?
+    # stupid_bot - insert artificial errors in preference extraction
+    # thorough - confirm every preference, how to do that? if any preference is detected
+    # on an utterance, confirm it? new node required?
+    return {
+        "enable_restart": {
+            "description": "Enable being able to restart the dialogue at any moment",
+            "is_enabled": False
+        },
+        "loud": {
+             "description": "OUTPUT IN ALL CAPS!!",
+             "is_enabled": False
+        },
+        "delayed": {
+             "description": "Introduce a delay before showing system responses",
+             "is_enabled": False
+        },
+        "voice_assistant": {
+             "description": "Enable text-to-speech for system utterances",
+             "is_enabled": False
+        }
+    }
+
+
+# combining sklearn models with our own baseline systems
+# enable user to select any when starting
+def create_models_dict(models):
+    return {
+        "majority_class": {
+            "description": "Label is always majority class of dataset",
+            "function": models[0]
+        },
+        "keyword_match": {
+            "description": "Matches keyword in utterances to classify dialogue",
+            "function": models[1]
+        },
+        "logistic_regression": {
+            "description": "Classifies utterance according to fit Logistic Regression model",
+            "function": models[2]
+        },
+        "multinomial_nb": {
+            "description": "Classifies utterance accornding to fit Multinomial Naive-Bayes model",
+            "function": models[3]
+        }
+    }
+
+
+def enable_method(models_dict, selected):
+    global classifier
+    key = list(models_dict)[selected[1]]
+    log.info(f"You've selected model {key}")
+
+    return models_dict[key]["function"]
+
+
+def show_options_menu(options, title, is_multi_select=False, min_multi=0):
+    log.debug(options)
+    s_list = []
+    for k, v in options.items():
+        desc = v["description"]
+        s_list.append(f"{k} - {desc}")
+
+    # return is list of tuples
+    return (pick(
+        options=s_list,
+        title=title,
+        multiselect=is_multi_select,
+        min_selection_count=min_multi
+    ))
+
+
+def get_confirmation_msg(form) -> str:
+    start = "You want to eat in a restaurant"
+    end = ", did I get that right?\n"
+    extras = ""
+
+    food = form["food"]
+    if food != "any" or "":
+        extras += f" that has {food} cuisine"
+    area = form["area"]
+    if area != "any" or "":
+        extras += f" somewhere around the {area}"
+    pricerange = form["pricerange"]
+    if pricerange != "any" or "":
+        extras += f" in the {pricerange} price range"
+
+    if food == "any" and area == "any" and pricerange == "any":
+        sys_utt = "All right! A surprise it is. Is that ok?\n"
+    else:
+        sys_utt = f"{start}{extras}{end}"
+
+    return sys_utt
